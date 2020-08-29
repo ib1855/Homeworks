7daa6225b8d7642af14f11cc2f849aebb475256d
Elias Bel <beilpa18.sup@gmail.com>
2020-08-30 01:58:06 +0600

    Add Spring Alishev MusicPlayer

---
+++ springAlishev/src/main/java/MusicPlayer.java
@@ -0,0 +1,36 @@
+public class MusicPlayer {
+    private String name;
+    private int volume;
+    private Music music;
+    public MusicPlayer(Music music){
+        this.music = music;
+    }
+
+    public MusicPlayer() {
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void playMusic(){
+        System.out.println("Playing: " + music.getSong());
+    }
+
+}
