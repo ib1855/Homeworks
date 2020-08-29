f1b82856dbaa70346765adcf27231444ff01fcc0
Elias Bel <beilpa18.sup@gmail.com>
2020-08-30 03:24:08 +0600

    MusicPlayer Task done

--- springAlishev/src/main/java/Main.java
+++ springAlishev/src/main/java/Main.java
@@ -5,10 +5,10 @@ public class Main {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-       MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-       musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+        //System.out.println(musicPlayer.getName());
+        //System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
--- springAlishev/src/main/java/MusicPlayer.java
+++ springAlishev/src/main/java/MusicPlayer.java
@@ -1,16 +1,18 @@
+import java.util.List;
+
 public class MusicPlayer {
     private String name;
     private int volume;
-    private Music music;
-    public MusicPlayer(Music music){
-        this.music = music;
+    private List<Music> musicList;
+    public MusicPlayer(List<Music> musicList){
+        this.musicList = musicList;
     }
 
     public MusicPlayer() {
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public String getName() {
@@ -30,7 +32,10 @@ public class MusicPlayer {
     }
 
     public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+        for (Music music: musicList
+             ) {
+            System.out.println("Playing: "  + music.getSong());
+        }
     }
 
 }
